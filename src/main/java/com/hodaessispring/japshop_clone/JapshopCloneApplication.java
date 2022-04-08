8b30789c021d902a7e5e5ddec8afef82e4a7c7ef
HodaeSsi <kfdream123@gmail.com>
2022-04-08 16:33:50 +0900

    Initial commit

---
+++ src/main/java/com/hodaessispring/japshop_clone/JapshopCloneApplication.java
@@ -0,0 +1,13 @@
+package com.hodaessispring.japshop_clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapshopCloneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JapshopCloneApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hodaessispring/japshop_clone/JapshopCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hodaessispring.japshop_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JapshopCloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
